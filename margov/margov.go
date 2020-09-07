ca138afb3b33ada0ffcd662b3ada74e3dc209c88
Simo Aleksandrov <simo3003@me.com>
2020-09-07 15:13:11 +0300

    impl chain.Set()

--- margov/margov.go
+++ margov/margov.go
@@ -13,10 +13,11 @@ func New() Chain {
 	return Chain{}
 }
 
-func (c Chain) Set(state State) {
+func (c Chain) Set(state State, otherState State, probability float64) {
 	// If the key state doesn't exist, initialize it
 	if _, ok := c[state]; !ok {
 		c[state] = Probabilities{}
 	}
 
+	c[state][otherState] = probability
 }
