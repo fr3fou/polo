b19f176b4d573cce10a965ee82aa73e24e86335c
Simo Aleksandrov <simo3003@me.com>
2020-09-07 15:10:42 +0300

    make chain recursive

--- margov/margov.go
+++ margov/margov.go
@@ -4,9 +4,16 @@ package margov
 type State = string
 
 // Chain is a Sequence of random states -> probabilities.
-type Chain map[State][][]float64
+type Chain map[State]Chain
 
 // New is a constructor of Chain
 func New() Chain {
 	return Chain{}
+}
+
+func (c Chain) Set(state State) {
+	// If the key state doesn't exist, initialize it
+	if _, ok := c[state]; !ok {
+		c[state] = New()
+	}
 }
