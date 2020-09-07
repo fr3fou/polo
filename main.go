0ecf17df5e49ee09419868352806dfaeb4c2ca23
Simo Aleksandrov <simo3003@me.com>
2020-09-07 15:14:32 +0300

    add weather consts

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"github.com/fr3fou/margov/margov"
+)
+
+const (
+	Sunny  = "Sunny"
+	Rainy  = "Rainy"
+	Cloudy = "Cloudy"
+)
 
 func main() {
-	fmt.Println("margov")
+
+	chain := margov.New()
+	chain.Set(Sunny, Sunny, 0.8)
+	chain.Set(Sunny, Sunny, 0.8)
 }
