c927f426e6b8999d6e22d920dfb7d524fe4cd248
Simo Aleksandrov <simo3003@me.com>
2020-09-07 15:15:36 +0300

    docs

--- margov/margov.go
+++ margov/margov.go
@@ -6,13 +6,15 @@ type State = string
 // Chain is a Sequence of random states -> probabilities.
 type Chain map[State]Probabilities
 
+// Probabilities gives the probabilities of going to the next state given some state.
 type Probabilities map[State]float64
 
-// New is a constructor of Chain
+// New is a constructor of Chain.
 func New() Chain {
 	return Chain{}
 }
 
+// Set sets the probability matrix of the current state to some next state.
 func (c Chain) Set(current State, next State, probability float64) {
 	// If the key state doesn't exist, initialize it
 	if _, ok := c[current]; !ok {
