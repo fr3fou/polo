7ccfa140d710f289b2eafc072f66d4e283291482
Simo Aleksandrov <simo3003@me.com>
2020-09-08 02:08:47 +0300

    Implement psuedo random number sampling

---
+++ rand/rand.go
@@ -0,0 +1,20 @@
+package rand
+
+import (
+	"math"
+	"math/rand"
+)
+
+// Sample is a (not 1:1) analog to `np.random.choice` with pseudo-random number sampling.
+func Sample(p []float64, accuracy float64) int {
+	intervals := []int{}
+
+	for i, probability := range p {
+		intervalSize := int(math.Round(accuracy * probability))
+		for j := 0; j < intervalSize; j++ {
+			intervals = append(intervals, i)
+		}
+	}
+
+	return intervals[rand.Intn(len(intervals))]
+}
