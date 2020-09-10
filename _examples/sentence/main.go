c0376269c2822c79cc4d9a896c78c10744d7470f
Simo Aleksandrov <simo3003@me.com>
2020-09-11 00:12:10 +0300

    make pairs from sentence

--- _examples/sentence/main.go
+++ _examples/sentence/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "github.com/fr3fou/margov/margov"
+import (
+	"fmt"
+	"strings"
+)
 
+/*
+	" ":          {"I"},
+	" I":         {"am"},
+	"I am":       {"a", "not"},
+	"a free":     {"man!"},
+	"am a":       {"free"},
+	"am not":     {"a"},
+	"a number!":  {"I"},
+	"number! I":  {"am"},
+	"not a":      {"number!"},
+*/
 func main() {
-	chain := margov.New()
+	// chain := margov.New(2)
+
+	f("I am not a number! I am a free man!")
+	//  I am a free man! I am not a number!
+}
+
+func f(str string) {
+	order := 2
+	text := " " + str
+	// occurrences := map[string]int{}
+	words := strings.Split(text, " ")
+	pairs := []string{}
+
+	for i := 0; i < len(words)-order; i++ {
+		pairs = append(pairs, strings.Join(words[i:i+order], " "))
+	}
+
+	for i := range pairs {
+		fmt.Println(pairs[i])
+	}
 }
