24ac800c2b7ad453e1500aa20a4b81ed1c826a33
Simo Aleksandrov <simo3003@me.com>
2020-09-10 22:31:31 +0300

    make Chain a struct instead of a map directly

---
+++ _examples/sentence/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/fr3fou/margov/margov"
+
+func main() {
+	chain := margov.New()
+}
--- margov/margov.go
+++ margov/margov.go
@@ -10,30 +10,34 @@ import (
 type State = string
 
 // Chain is a Sequence of random states -> probabilities.
-type Chain map[State]Probabilities
+type Chain struct {
+	Probabilities map[State]Probabilities
+}
 
 // Probabilities gives the probabilities of going to the next state given some state.
 type Probabilities map[State]float64
 
 // New is a constructor of Chain.
 func New() Chain {
-	return Chain{}
+	return Chain{
+		Probabilities: map[string]Probabilities{},
+	}
 }
 
 // Set sets the probability matrix of the current state to some next state.
 func (c Chain) Set(next State, current State, probability float64) {
 	// If the key state doesn't exist, initialize it
-	if _, ok := c[current]; !ok {
-		c[current] = Probabilities{}
+	if _, ok := c.Probabilities[current]; !ok {
+		c.Probabilities[current] = Probabilities{}
 	}
 
-	c[current][next] = probability
+	c.Probabilities[current][next] = probability
 }
 
 func (c Chain) String() string {
 	sb := strings.Builder{}
 
-	for current, matrix := range c {
+	for current, matrix := range c.Probabilities {
 		for next, probability := range matrix {
 			sb.WriteString(fmt.Sprintf("P(%s|%s) = %.02f\n", next, current, probability))
 		}
@@ -45,7 +49,7 @@ func (c Chain) String() string {
 
 // Probability returns the probability of the next state happening given the current one.
 func (c Chain) Probability(next State, current State) float64 {
-	return c[current][next]
+	return c.Probabilities[current][next]
 }
 
 // Next gives the next state given the current state.
@@ -53,7 +57,7 @@ func (c Chain) Next(current State) State {
 	probs := []float64{}
 	states := []State{}
 
-	for state, probability := range c[current] {
+	for state, probability := range c.Probabilities[current] {
 		probs = append(probs, probability)
 		states = append(states, state)
 	}
