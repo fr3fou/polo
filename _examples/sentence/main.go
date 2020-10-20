919dee18b057873f930e201d5c389b7bd85f9e05
Simo Aleksandrov <simo3003@me.com>
2020-10-20 23:37:46 +0300

    Implement predictRandom

--- _examples/sentence/main.go
+++ _examples/sentence/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"os"
 	"path/filepath"
 	"strings"
@@ -24,6 +25,10 @@ func main() {
 		panic("not enough args, provide path to fb inbox dir")
 	}
 	dir := os.Args[1]
+	var start string
+	if len(os.Args) == 3 {
+		start = os.Args[2]
+	}
 
 	sentences := []string{}
 
@@ -63,7 +68,11 @@ func main() {
 	order := 1
 	occurrences := buildOccurrences(sentences, order)
 	chain := createChain(occurrences, order)
-	fmt.Println(predict(chain, "kvo", 10))
+	if start == "" {
+		fmt.Println(predictRandom(chain, 10))
+	} else {
+		fmt.Println(predict(chain, start, 10))
+	}
 }
 
 func buildOccurrences(sentences []string, order int) map[string]map[string]int {
@@ -101,6 +110,14 @@ func createChain(m map[string]map[string]int, order int) margov.Chain {
 }
 
 func predictRandom(c margov.Chain, n int) string {
+	i := rand.Intn(len(c.StateTransitions))
+	for k := range c.StateTransitions {
+		if i == 0 {
+			return predict(c, k, n)
+		}
+		i--
+	}
+
 	return ""
 }
 
